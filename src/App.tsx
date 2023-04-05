6542bd853e691c6f55f6d13a07c0f9da27749834
samlogy <aceportgasd235@gmail.com>
2023-04-05 13:29:05 +0100

    remove: app test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import "./App.css";
+import Counter from "./components/Counter";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
 }
